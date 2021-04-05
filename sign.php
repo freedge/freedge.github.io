073e7d303d84ff8bdfce8ae354c20218ab7d3afc
François Rigault <rigault.francois@gmail.com>
2021-04-05 20:17:32 +0200

    Downgrade everything to support php56

--- sign.php
+++ sign.php
@@ -1,31 +1,21 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 use HttpSignatures\Context;
-
-$psr17Factory = new \Nyholm\Psr7\Factory\Psr17Factory();
-
-$creator = new \Nyholm\Psr7Server\ServerRequestCreator(
-    $psr17Factory, // ServerRequestFactory
-    $psr17Factory, // UriFactory
-    $psr17Factory, // UploadedFileFactory
-    $psr17Factory  // StreamFactory
-);
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\JsonResponse;
+use Zend\Diactoros\Response\SapiEmitter;
 
 $body = array("hello" => "world");
-$serverRequest = $creator->fromGlobals();
 
-$responseBody = $psr17Factory->createStream(json_encode($body));
-$response = $psr17Factory->createResponse(200)
-  ->withBody($responseBody);
+$response = new JsonResponse($body);
 $context = new HttpSignatures\Context([
-    'keys' => ['mykey' => file_get_contents(__DIR__ . '../.secret/key.pem')],
-    'algorithm' => 'rsa-sha256',
-    'headers' => ['(request-target)', 'Date'],
-  ]);
-$context->signer()->signWithDigest($response);
+            'keys' => ['mykey' => file_get_contents(__DIR__ . '/../.secret/key.pem')],
+	    'algorithm' => 'rsa-sha256',
+	    'headers' => [],
+]);
+$context->signer()->sign($response);
 
 
-(new \Laminas\HttpHandlerRunner\Emitter\SapiEmitter())->emit($response);
-
+(new SapiEmitter())->emit($response);
 
 ?>
