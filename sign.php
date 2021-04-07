e44d0ebb94b53c15bf0579561fbc3788fbb8a291
François Rigault <rigault.francois@gmail.com>
2021-04-07 06:39:40 +0200

    Use PHP-CS-Fixer to lint code
    Try to follow some best practices for the rest of the pages as well

--- sign.php
+++ sign.php
@@ -1,29 +1,28 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
-use HttpSignatures\Context;
-use Zend\Diactoros\ServerRequestFactory;
+
+require __DIR__.'/vendor/autoload.php';
 use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\Response\SapiEmitter;
 
 // generate a nice body
-$body = array(
-  "request" => "sign.php",
-  "ip" => $_SERVER['REMOTE_ADDR']
-);
+$body = [
+    'request' => 'sign.php',
+    'ip' => $_SERVER['REMOTE_ADDR'],
+];
 
 try {
-  $body["pubkey"] = file_get_contents(__DIR__ . '/../.secret/pubkey');
+    $body['pubkey'] = file_get_contents(__DIR__.'/../.secret/pubkey');
 } catch (Exception $e) {
-  $body["error"] = $e->getMessage();
+    $body['error'] = $e->getMessage();
 }
 
-$response = new JsonResponse($body, 200, array("date" => gmdate('D, d M Y H:i:s T')));
+$response = new JsonResponse($body, 200, ['date' => gmdate('D, d M Y H:i:s T')]);
 
 // add the signature
 $context = new HttpSignatures\Context([
-            'keys' => ['mykey' => file_get_contents(__DIR__ . '/../.secret/key.pem')],
-	    'algorithm' => 'rsa-sha256',
-	    'headers' => ['Date'],
+    'keys' => ['mykey' => file_get_contents(__DIR__.'/../.secret/key.pem')],
+    'algorithm' => 'rsa-sha256',
+    'headers' => ['Date'],
 ]);
 
 $response = $context->signer()->signWithDigest($response);
@@ -31,8 +30,5 @@ $response = $context->signer()->signWithDigest($response);
 // can be verified with
 //   $context->verifier()->isSigned($response)
 
-
 // send the response
 (new SapiEmitter())->emit($response);
-
-?>
